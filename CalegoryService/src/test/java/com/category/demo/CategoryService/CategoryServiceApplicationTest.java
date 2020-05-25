925e3d9fd3243c3749d664c6c1aaad6b9e4312a3
guru <guru@host.docker.internal>
2020-05-25 12:00:48 +0530

    Test class name updated

---
+++ CalegoryService/src/test/java/com/category/demo/CategoryService/CategoryServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.category.demo.CategoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest
+class CategoryServiceApplicationTest {
+	
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
